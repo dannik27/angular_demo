71a5f8efbb9713bb4a9345fb94e975a5084e196f
dannik27 <nikifordan@gmail.com>
2018-03-14 08:13:40 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { MapsListComponent } from './maps-list/maps-list.component';
+import { RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent,
+    MapsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'map-list',
+      component: MapsListComponent
+    }, {
+      path: 'map',
+      component: MapComponent
+    }]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
